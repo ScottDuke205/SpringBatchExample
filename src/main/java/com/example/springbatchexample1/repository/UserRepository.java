516a19ad0e63f7c59fdcf88e940e9d18bc070da4
ScottDuke205 <snd1964@gmail.com>
2021-06-10 14:45:03 -0500

    Removed @Repository on UserRepository class.

--- src/main/java/com/example/springbatchexample1/repository/UserRepository.java
+++ src/main/java/com/example/springbatchexample1/repository/UserRepository.java
@@ -4,6 +4,5 @@ import com.example.springbatchexample1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 }
